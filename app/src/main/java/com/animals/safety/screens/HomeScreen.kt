f1212557fde283e3e38c4326e44653c1ef698983
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-07-27 10:49:46 +0200

    refactor(ui): reuse string resource and add previews

--- app/src/main/java/com/animals/safety/screens/HomeScreen.kt
+++ app/src/main/java/com/animals/safety/screens/HomeScreen.kt
@@ -25,6 +25,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import com.animals.safety.R
 import com.animals.safety.data.Animal
@@ -106,7 +108,7 @@ private fun HomeCell(
   ) {
     Image(
       painter = painterResource(animal.breed.cover),
-      contentDescription = stringResource(animal.breed.translatedName),
+      contentDescription = breedTranslatedName,
       modifier = Modifier.size(64.dp)
     )
     Column(
@@ -127,6 +129,8 @@ private fun HomeCell(
 }
 
 @Preview(showBackground = true)
+@PreviewLightDark
+@PreviewScreenSizes
 @Composable
 private fun HomeCellPreview() {
   AimantsDanimauxTheme(dynamicColor = false) {
