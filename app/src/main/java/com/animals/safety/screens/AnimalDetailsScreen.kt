fe086cfef04b30c42f3422e959359eec64c5a631
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-07-27 10:35:25 +0200

    refactor(ui): replace fixed height with aspect ratio for cover image

--- app/src/main/java/com/animals/safety/screens/AnimalDetailsScreen.kt
+++ app/src/main/java/com/animals/safety/screens/AnimalDetailsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -89,7 +90,7 @@ private fun AnimalDetails(
         contentDescription = stringResource(animal.breed.translatedName),
         modifier = Modifier
           .fillMaxWidth()
-          .size(300.dp),
+          .aspectRatio(4/3f),
         contentScale = ContentScale.Crop,
       )
       Text(
